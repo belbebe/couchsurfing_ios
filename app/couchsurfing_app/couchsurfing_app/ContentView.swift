33c7575f2e9b532832567df7c5d3f9a4f56f457a
Belcsik Bence <belcsik.bebe09@gmail.com>
2022-03-10 03:03:28 +0100

    app view elkezdése, haladási napló frissítése eddigi ütemtervvel

---
+++ app/couchsurfing_app/couchsurfing_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  couchsurfing_app
+//
+//  Created by Belcsik Bence on 2022. 03. 03..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/couchsurfing_app/couchsurfing_app/couchsurfing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  couchsurfing_appApp.swift
+//  couchsurfing_app
+//
+//  Created by Belcsik Bence on 2022. 03. 03..
+//
+
+import SwiftUI
+
+@main
+struct couchsurfing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
