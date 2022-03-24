de6bd7692703591fc210215b4f469e86eea582a3
Belcsik Bence <belcsik.bebe09@gmail.com>
2022-03-24 03:36:25 +0100

    "Foglalás" tab kész.

--- app/couchsurfing_app/couchsurfing_app/ContentView.swift
+++ app/couchsurfing_app/couchsurfing_app/ContentView.swift
@@ -10,6 +10,8 @@ import Combine
 
 struct ContentView: View {
     
+    @Environment(\.defaultMinListRowHeight) var minRowHeight
+    
     @State private var foglalas_varos: String = ""
     @State private var foglalas_kezdete = Date()
     @State private var foglalas_vege = Date()
@@ -18,126 +20,155 @@ struct ContentView: View {
     @State private var ar_ig = "0"
     @State private var ertekeles_tol = "0"
     @State private var ertekeles_ig = "0"
-    @State private var egyeb_igeny_items: [String] = ["Nem-dohányzó", "Állatbarát", "Légkondícionáló", "Parkolóhely biztosított"]
-    @State private var egyeb_igeny_selections: [String] = []
+    @State private var egyeb_igeny = [
+        EgyebIgeny(name: "Nem-dohányzó"),
+        EgyebIgeny(name: "Állatbarát"),
+        EgyebIgeny(name: "Légkondíciónáló"),
+        EgyebIgeny(name: "Parkolóhely biztsoított"),
+        EgyebIgeny(name: "Bicikli tárolás biztosított")
+    ]
+    //@State private var egyeb_igeny_items: [String] = ["Nem-dohányzó", "Állatbarát", "Légkondícionáló", "Parkolóhely biztosított"]
+    //@State private var egyeb_igeny_selections: [String] = []
+    @State private var egyeb_igeny_selections = Set<UUID>()
+    
+    private struct EgyebIgeny: Identifiable, Hashable {
+        let name: String
+        let id = UUID()
+    }
     
     var body: some View {
         //TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
         TabView {
-            VStack(spacing: 30) {
-                Text("Foglalás")
-                    .font(.system(size: 40, weight: .bold))
-                
-                TextField("Város", text: $foglalas_varos)
-                    .frame(width: 300, height: 30, alignment: .center)
-                
-                HStack(spacing: 50) {
-                    VStack {
-                        Text("Tartózkodás kezdete")
+            // "Foglalás" tab
+            NavigationView {
+                ScrollView {
+                    VStack(spacing: 30) {
+                        Text("Foglalás")
+                            .font(.system(size: 40, weight: .bold))
+                        
+                        TextField("Város", text: $foglalas_varos)
+                            .frame(width: 300, height: 30, alignment: .center)
+                        
+                        HStack(spacing: 50) {
+                            VStack {
+                                Text("Tartózkodás kezdete")
+                                    .font(.system(size: 14, weight: .bold))
+                                DatePicker("Tartózkodás kezdete", selection: $foglalas_kezdete, displayedComponents: [.date])
+                                    .frame(width: 125, height: 30, alignment: .center)
+                            }
+                            VStack {
+                                Text("Tartózkodás vége")
+                                    .font(.system(size: 14, weight: .bold))
+                                DatePicker("Tartózkodás kezdete", selection: $foglalas_vege, displayedComponents: [.date])
+                                    .frame(width: 125, height: 30, alignment: .center)
+                            }
+                        }
+                        
+                        Text("Fizetés módja")
                             .font(.system(size: 14, weight: .bold))
-                        DatePicker("Tartózkodás kezdete", selection: $foglalas_kezdete, displayedComponents: [.date])
-                            .frame(width: 125, height: 30, alignment: .center)
-                    }
-                    VStack {
-                        Text("Tartózkodás vége")
+                        
+                        Picker(selection: $fizetes_modja, label: Text("Fizetés módja")) {
+                            Text("Bankkártya").tag(1)
+                            Text("Készpénz").tag(2)
+                            Text("Ellenszolgáltatás").tag(3)
+                        }
+                        
+                        Text("Preferált árkategória")
                             .font(.system(size: 14, weight: .bold))
-                        DatePicker("Tartózkodás kezdete", selection: $foglalas_vege, displayedComponents: [.date])
-                            .frame(width: 125, height: 30, alignment: .center)
-                    }
-                }
-                
-                Text("Fizetés módja")
-                    .font(.system(size: 14, weight: .bold))
-                
-                Picker(selection: $fizetes_modja, label: Text("Fizetés módja")) {
-                    Text("Bankkártya").tag(1)
-                    Text("Készpénz").tag(2)
-                    Text("Ellenszolgáltatás").tag(3)
-                }
-                
-                Text("Preferált árkategória")
-                    .font(.system(size: 14, weight: .bold))
-                
-                HStack(spacing: 50) {
-                    VStack {
-                        Text("-tól")
-                            .font(.system(size: 12))
-                        TextField("-tól", text: $ar_tol)
-                            .keyboardType(.numberPad)
-                            .onReceive(Just(ar_tol)) { newValue in
-                                let filtered = newValue.filter { "0123456789".contains($0) }
-                                if filtered != newValue {
-                                    self.ar_tol = filtered
-                                }
+                        
+                        HStack(spacing: 50) {
+                            VStack {
+                                Text("-tól")
+                                    .font(.system(size: 12))
+                                TextField("0", text: $ar_tol)
+                                    .keyboardType(.numberPad)
+                                    .onReceive(Just(ar_tol)) { newValue in
+                                        let filtered = newValue.filter { "0123456789".contains($0) }
+                                        if filtered != newValue {
+                                            self.ar_tol = filtered
+                                        }
+                                    }
                             }
-                    }
-                    VStack {
-                        Text("-ig")
-                            .font(.system(size: 12))
-                        TextField("-ig", text: $ar_ig)
-                            .keyboardType(.numberPad)
-                            .onReceive(Just(ar_ig)) { newValue in
-                                let filtered = newValue.filter { "0123456789".contains($0) }
-                                if filtered != newValue {
-                                    self.ar_ig = filtered
-                                }
+                            VStack {
+                                Text("-ig")
+                                    .font(.system(size: 12))
+                                TextField("0", text: $ar_ig)
+                                    .keyboardType(.numberPad)
+                                    .onReceive(Just(ar_ig)) { newValue in
+                                        let filtered = newValue.filter { "0123456789".contains($0) }
+                                        if filtered != newValue {
+                                            self.ar_ig = filtered
+                                        }
+                                    }
                             }
-                    }
-                }
-                
-                Text("Preferált értékelés")
-                    .font(.system(size: 14, weight: .bold))
-                
-                HStack(spacing: 50) {
-                    VStack {
-                        Text("-tól")
-                            .font(.system(size: 12))
-                        TextField("-tól", text: $ertekeles_tol)
-                            .keyboardType(.numberPad)
-                            .onReceive(Just(ertekeles_tol)) { newValue in
-                                let filtered = newValue.filter { "0123456789".contains($0) }
-                                if filtered != newValue {
-                                    self.ertekeles_tol = filtered
-                                }
+                        }
+                        
+                        Text("Preferált értékelés")
+                            .font(.system(size: 14, weight: .bold))
+                        
+                        HStack(spacing: 50) {
+                            VStack {
+                                Text("-tól")
+                                    .font(.system(size: 12))
+                                TextField("0", text: $ertekeles_tol)
+                                    .keyboardType(.numberPad)
+                                    .onReceive(Just(ertekeles_tol)) { newValue in
+                                        let filtered = newValue.filter { "0123456789".contains($0) }
+                                        if filtered != newValue {
+                                            self.ertekeles_tol = filtered
+                                        }
+                                    }
                             }
-                    }
-                    VStack {
-                        Text("-ig")
-                            .font(.system(size: 12))
-                        TextField("-ig", text: $ertekeles_ig)
-                            .keyboardType(.numberPad)
-                            .onReceive(Just(ertekeles_ig)) { newValue in
-                                let filtered = newValue.filter { "0123456789".contains($0) }
-                                if filtered != newValue {
-                                    self.ertekeles_ig = filtered
-                                }
+                            VStack {
+                                Text("-ig")
+                                    .font(.system(size: 12))
+                                TextField("0", text: $ertekeles_ig)
+                                    .keyboardType(.numberPad)
+                                    .onReceive(Just(ertekeles_ig)) { newValue in
+                                        let filtered = newValue.filter { "0123456789".contains($0) }
+                                        if filtered != newValue {
+                                            self.ertekeles_ig = filtered
+                                            print("\(self.ertekeles_ig)")
+                                        }
+                                    }
                             }
-                    }
-                }
-                
-                Text("Egyéb igények")
-                    .font(.system(size: 14, weight: .bold))
-                
-                /*
-                List {
-                    ForEach(self.egyeb_igeny_items, id: \.self) { item in
-                        MultipleSelectionRow(title: item, isSelected: self.egyeb_igeny_selections.contains(item)) {
-                            if self.egyeb_igeny_selections.contains(item) {
-                                self.egyeb_igeny_selections.removeAll(where: { $0 == item })
+                        }
+                        
+                        Group {
+                            Text("Egyéb igények")
+                                .font(.system(size: 14, weight: .bold))
+                            
+                            List(egyeb_igeny, selection: $egyeb_igeny_selections) {
+                                Text($0.name)
                             }
-                            else {
-                                self.egyeb_igeny_selections.append(item)
+                            .frame(minHeight: minRowHeight * 3)
+                            .toolbar {EditButton()}
+                            
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                Text("Keresés")
                             }
+                            .border(.blue)
                         }
                     }
                 }
-                */
-                
             }
             .tabItem { VStack {
                 Text("Foglalás")
                 Image(systemName: "calendar")
             } }.tag(1)
+            
+            
+            // "Szoba kiadása" tab
+            NavigationView {
+                ScrollView {
+                    
+                }
+            }
+            .tabItem {
+                VStack {
+                    Label("Szoba kiadása", systemImage: "house")
+                }
+            }
         }
     }
 }
